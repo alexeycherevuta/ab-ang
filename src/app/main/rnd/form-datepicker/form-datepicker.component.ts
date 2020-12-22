35c46378a96cb7075b0723b60b65b8fa74139e1f
Alexey <alexey.cherevuta@devit-team.com>
2020-12-22 13:15:38 +0000

    - update datepicker & datetimepicker directive - update button submit input mask rnd page

--- src/app/main/rnd/form-datepicker/form-datepicker.component.ts
+++ src/app/main/rnd/form-datepicker/form-datepicker.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit, Injector, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import * as moment from 'moment';
 @Component({
   templateUrl: './form-datepicker.component.html'
 })
 export class FormDatepickerComponent extends AppComponentBase  implements OnInit {
-  dateSelected: any;
+  datepickSelected: any;
+  datetimepickSelected: any;
   constructor(
     injector: Injector,) 
   { 
     super(injector)
   }
   ngOnInit() {
+  }
+  save(): void {
+    console.log(this.datepickSelected + " "+ this.datetimepickSelected);
   }
 }
