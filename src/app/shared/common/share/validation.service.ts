4e405800508ec139a54a44f1e89ad2bb7230b79d
Alexey <alexey.cherevuta@devit-team.com>
2021-02-17 14:33:43 +0000

    update regex email validationservice

--- src/app/shared/common/share/validation.service.ts
+++ src/app/shared/common/share/validation.service.ts
@@ -18,7 +18,7 @@ export class ValidationService {
         return errorMessage;
     }
     static emailCompleteValidator(input) {
-        let regex = new RegExp(/^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]+[A-Za-z0-9]([A-Za-z0-9-]+[A-Za-z0-9])?(\.([A-Za-z0-9-]{1,61}[A-Za-z0-9])?)*$/);
+        let regex = new RegExp(/^[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}$/);    
         if (regex.test(input.value)) {
             return null;
         } else {
