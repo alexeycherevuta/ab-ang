5698f98ee1b609f91466f8028b3963544d7d4be3
Alexey <alexey.cherevuta@devit-team.com>
2021-02-21 03:39:50 +0000

    update datepicker directive format and moment helper appcomponentbase

--- src/app/shared/common/timing/date-picker.component.ts
+++ src/app/shared/common/timing/date-picker.component.ts
@@ -1,7 +1,4 @@
-import {
-    Directive, AfterViewInit, ElementRef, ViewChild, Injector, Input, Output, EventEmitter,
-    HostListener, OnChanges
-} from '@angular/core';
+import { Directive, AfterViewInit, ElementRef, ViewChild, Injector, Input, Output, EventEmitter, HostListener, OnChanges } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import * as moment from 'moment';
 import {NgModel} from "@angular/forms";
@@ -22,7 +19,7 @@ export class DatePickerDirective extends AppComponentBase implements AfterViewIn
         this._selectedDate = val;
         this.selectedDateChange.emit(this._selectedDate);
         this.setElementText(val);
-        this.ngModelChange.emit(this._selectedDate.format('YYYY-MM-DD'));
+        this.ngModelChange.emit(this._selectedDate.format('DD/MM/YYYY'));
     }
     constructor(
         injector: Injector,
@@ -34,7 +31,8 @@ export class DatePickerDirective extends AppComponentBase implements AfterViewIn
     ngAfterViewInit(): void {
         const $element = $(this.hostElement.nativeElement);
         $element.datepicker({
-            language: abp.localization.currentLanguage.name
+            language: abp.localization.currentLanguage.name,
+            format: "dd/mm/yyyy"
         }).on('changeDate', e => {
             this.selectedDate = moment(e.date);
         }).on('clearDate', e => {
@@ -44,7 +42,7 @@ export class DatePickerDirective extends AppComponentBase implements AfterViewIn
     setElementText(val: any) {
         const $element = $(this.hostElement.nativeElement);
         if (val) {
-            $element.val(moment(val).format('L'));
+            $element.val(moment(val).format('DD/MM/YYYY'));
         } else {
             $element.val('');
         }
