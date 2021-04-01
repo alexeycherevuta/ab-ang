ae275f2f5fb6b4347ce683502580db8ce7ab4c2d
Alexey <alexey.cherevuta@devit-team.com>
2021-04-01 05:02:11 +0000

    update example complex ddl share comp

--- src/app/main/share/rnd/example-complex-ddl.component.ts
+++ src/app/main/share/rnd/example-complex-ddl.component.ts
@@ -5,8 +5,7 @@ import {
 import { PermissionServiceProxy, FlatPermissionWithLevelDto, RoleServiceProxy } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
-const noop = () => {
-};
+const noop = () => {};
 export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
     provide: NG_VALUE_ACCESSOR,
     useExisting: forwardRef(() => ExampleComplexDdlComponent),
@@ -15,12 +14,12 @@ export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
 @Component({
     selector: 'example-complex-ddl',
     template: `<select #DropdownList
-                class="form-control"
-                [(ngModel)]="inputValue"
-                [attr.data-live-search]="true">
+                    class="form-control"
+                    [(ngModel)]="inputValue"
+                    [attr.data-live-search]="true">
                     <option [ngValue]="undefined">{{l('NothingSelected')}}</option>
                     <option *ngFor="let a of valueList" [ngValue]="a.id">{{a.displayName}}</option>
-            </select>`,
+                </select>`,
     providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
 export class ExampleComplexDdlComponent extends AppComponentBase implements ControlValueAccessor, OnInit, AfterViewInit {
@@ -49,12 +48,11 @@ export class ExampleComplexDdlComponent extends AppComponentBase implements Cont
         super(injector);
     }
     ngOnInit(): void {
-        let self = this;
         this._roleService.getRoles(undefined).subscribe(result => {
             this.valueList = result.items;
             this.refreshAll();
         }, err => {
-            console.error("ExampleComplexDdlComponent: request service roleService.getRoles() - line 86");
+            console.error("ExampleComplexDdlComponent: request service roleService.getRoles()");
             this.refreshAll();
         });
     }
@@ -65,9 +63,8 @@ export class ExampleComplexDdlComponent extends AppComponentBase implements Cont
         });
     }
     refreshAll(): void {
-        let self = this;
         setTimeout(() => {
-            $(self.dropdownListElement.nativeElement).selectpicker('refresh');
+            $(this.dropdownListElement.nativeElement).selectpicker('refresh');
         }, 0);
     }
     get inputValue(): any {
